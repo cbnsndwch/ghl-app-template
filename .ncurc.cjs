53c2474ba4df37b28e707d08e7a056912393a29a
Sergio Leon <8313760+cbnsndwch@users.noreply.github.com>
2024-05-14 19:03:56 -0400

    chore(deps): bump dependencies

--- .ncurc.cjs
+++ .ncurc.cjs
@@ -1,7 +1,7 @@
 /**
  * Put the names of packages you don't want NCU to suggest updates for here.
  */
-const reject = ['eslint'];
+const reject = ['eslint', 'conventional-changelog-cli'];
 
 /**
  * @type {import('npm-check-updates').RunOptions}
